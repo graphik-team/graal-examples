da8a6ad6f7863c1251c249a306174ca08ed9d4e9
Clément SIPIETER <clement@6pi.fr>
2017-06-30 09:17:29 +0200

    update script

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -1,16 +1,20 @@
 #!/bin/sh
 
 cd $1
-mvn clean package
+mvn clean
+cd -
+zip graal-$1-maven-example.zip $1/*
+cd $1
+mvn package
 cd -
 
-rm -rf /tmp/graal-$1-example/
-mkdir /tmp/graal-$1-example/
-cp -r $1/data/ /tmp/graal-$1-example/
-cp -r $1/src/main/java/fr/ /tmp/graal-$1-example/
-cp    $1/target/graal-$1-*.jar /tmp/graal-$1-example/
+$tmpdir=$(mktemp)
+mkdir $tmpdir/graal-$1-example/
+cp -r $1/data/ $tmpdir/graal-$1-example/
+cp -r $1/src/main/java/fr/ $tmpdir/graal-$1-example/
+cp    $1/target/graal-$1-*.jar $tmpdir/graal-$1-example/
 
 current_dir=$(pwd)
-cd /tmp/
+cd $tmpdir
 zip -r $current_dir/graal-$1-example.zip ./graal-$1-example/*
 cd -
