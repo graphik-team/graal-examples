66e8af943c70b30b80c6eb1bd55d82f7b97346ee
Clément SIPIETER <clement@6pi.fr>
2018-02-09 17:19:19 +0100

    update build-package script

--- scripts/build-package.sh
+++ scripts/build-package.sh
@@ -8,13 +8,14 @@ cd $1
 mvn -q package
 cd -
 
-$tmpdir=$(mktemp)
+tmpdir=$(mktemp -d)
 mkdir $tmpdir/graal-$1-example/
 cp -r $1/data/ $tmpdir/graal-$1-example/
 cp -r $1/src/main/java/fr/ $tmpdir/graal-$1-example/
-cp    $1/target/graal-$1-*.jar $tmpdir/graal-$1-example/
+cp    $1/target/$1-*.jar $tmpdir/graal-$1-example/
 
 current_dir=$(pwd)
 cd $tmpdir
 zip -r $current_dir/graal-$1-example.zip ./graal-$1-example/*
 cd -
+
